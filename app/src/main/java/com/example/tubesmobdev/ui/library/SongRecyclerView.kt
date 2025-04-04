91c8a7d526c745ebc52196484642e0673b4ccda2
Enrique Yanuar <enriqueyanuar.7@gmail.com>
2025-04-04 23:49:29 +0700

    fix: add comment

--- app/src/main/java/com/example/tubesmobdev/ui/library/SongRecyclerView.kt
+++ app/src/main/java/com/example/tubesmobdev/ui/library/SongRecyclerView.kt
@@ -11,6 +11,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.tubesmobdev.data.model.Song
 import com.example.tubesmobdev.ui.library.adapter.SongAdapter
 
+
+/**
+ * A composable that wraps a RecyclerView using XML and your SongAdapter.
+ * @param songs The list of songs to display.
+ * @param onItemClick A lambda invoked when an item is clicked.
+ */
 @Composable
 fun SongRecyclerView(songs: List<Song>, onItemClick: (Song) -> Unit) {
     AndroidView(
