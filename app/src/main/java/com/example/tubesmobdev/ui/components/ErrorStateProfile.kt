00c5ecaa977cac3147a7c751d2055041680e91b7
Otzzu <mesachharmasendro@gmail.com>
2025-04-11 13:48:27 +0700

    feat: make text center in error profile

--- app/src/main/java/com/example/tubesmobdev/ui/components/ErrorStateProfile.kt
+++ app/src/main/java/com/example/tubesmobdev/ui/components/ErrorStateProfile.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
@@ -15,6 +16,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -29,7 +31,9 @@ fun ErrorStateProfile(message: String, onLogout: () -> Unit) {
         Text(
             text = message,
             style = MaterialTheme.typography.headlineSmall,
-            color = Color.White
+            color = Color.White,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.fillMaxWidth()
         )
         Spacer(modifier = Modifier.height(24.dp))
         Button(
